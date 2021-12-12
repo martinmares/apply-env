fe60d387ada0d8d47cbe19c0250c085e60e7c37b
Martin Mareš <mares@datalite.cz>
2021-12-12 11:46:10 +0100

    Add parse ENV, render

--- src/apply_env.cr
+++ src/apply_env.cr
@@ -4,16 +4,51 @@ require "option_parser"
 module ApplyEnv
   VERSION = "1.0.0"
 
+  class EnvMatch
+    getter :orig, :name, :value, :found
+
+    @name : String = ""
+    @value : String = ""
+    @found : Bool = false
+
+    def initialize(@orig : String)
+      parse(@orig)
+    end
+
+    def found?
+      @found
+    end
+
+    private def parse(orig : String)
+      match = /(\{\{\s*\.Env\.)(\w+)(\s*\}\})/ix.match(orig)
+      if match
+        @name = match[2] if match[2]?
+        if ENV.has_key? @name
+          @value = ENV[@name]
+          @found = true
+        end
+      end
+    end
+  end
+
   class Template
-    getter :file_name, :content
+    getter :file_name, :content, :new_content
 
     @file_name : String
+    @debug : Bool
     @content : String
+    @env_matches : Array(EnvMatch)
 
     def initialize()
       opts = parse_opts()
       @file_name = opts[:file_name]
+      @debug = if opts.has_key?(:debug)
+        opts[:debug].downcase.starts_with?("true")
+      else
+        false
+      end
       @content = ""
+      @env_matches = Array(EnvMatch).new
     end
 
     def parse_opts : Hash(Symbol, String)
@@ -21,6 +56,8 @@ module ApplyEnv
       OptionParser.parse do |parser|
         parser.banner = "Usage: apply_env [arguments]"
         parser.on("-f NAME", "--file=NAME", "Specifies template file name") { |name| result[:file_name] = name }
+        parser.on("-d", "--debug", "Debug?") { result[:debug] = "true" }
+        # parser.on("-d DEBUG", "--debug=DEBUG", "Debug? [true || false]") { |debug| result[:debug] = debug }
         parser.on("-v", "--version", "App version") do
           puts "App name: apply_env"
           puts "App version: #{VERSION}"
@@ -35,41 +72,55 @@ module ApplyEnv
           STDERR.puts parser
           exit(1)
         end
+  
+      end
+      unless result.has_key?(:file_name)
+        puts "File (name must be specified!".colorize(:red)
+        exit(1)
       end
       result
     end
 
     def find_env_matches : Array(String)
       result = Array(String).new
-      @content.scan(/\{\{\s*\.Env\.\w+\s*\}\}/ix).each do |match|
-        result << match[0] if match.size == 1
+      if @content
+        @content.scan(/\{\{\s*\.Env\.\w+\s*\}\}/ix).each do |match|
+          result << match[0] if match.size == 1
+        end
       end
       result
     end
 
     def exists?
-      File.exists? @file_name unless @file_name.nil?
+      File.exists? @file_name
     end
 
-    def load_content
+    def load_content : String
       if exists?
         @content = File.read(@file_name)
+      else
+        @content = ""
       end
     end
 
-    def render
-      load_content()
-      matches = find_env_matches()
-      matches.each do |m|
-        puts m
+    def render : String
+      @content = load_content()
+      env_matches = find_env_matches()
+      env_matches.each_with_index do |m, i|
+        env_match = EnvMatch.new(m)
+        @env_matches << env_match
+        puts "Found [#{i}], orig: \"#{m.to_s.colorize(:yellow)}\", env_var: #{env_match.value.colorize(:green)}" if @debug
       end
+      new_content = @content
+      @env_matches.each do |env_match|
+        new_content = new_content.gsub(env_match.orig, env_match.value) if env_match.found?
+      end
+      new_content
     end
 
   end
 
   template = Template.new() # "#{t1}, #{t2}"
-  template.render()
-  # matches = template.find_env_matches()
-  # puts "Reading content from template: #{template.file_name.to_s.colorize(:cyan)}"
-  # puts "Found #{matches.size.to_s.colorize(:green)} strings to replace"
+  result = template.render()
+  puts result
 end
